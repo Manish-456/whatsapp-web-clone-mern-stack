d5e8cc27277a2f2e85b893491ba73960e6b791eb
Manish-456 <tamangmanish3456@gmail.com>
2023-02-05 15:02:08 +0545

    new env file created

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import './index.css';
-const clientId =
-  "943923581060-kituu8m1vfjvotjo27lan60rb1oa8ds6.apps.googleusercontent.com";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <GoogleOAuthProvider clientId={clientId}>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
     <AuthProvider>
       <App />
     </AuthProvider>
